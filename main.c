6c5a6de16aeff0acc820c80a0db3b36632539528
yeah-right-yeah-right <124061664+yeah-right-yeah-right@users.noreply.github.com>
2025-10-12 19:55:04 +0200

    debug temps

--- main.c
+++ main.c
@@ -10,30 +10,37 @@
 int main(void){
 
     //fibo exo 1
+    float ms;
     clock_t begin = clock();
     printf("naif: %i\n", fibo_naif(5));
     clock_t end = clock();
-    unsigned long temps = (double)(end-begin)/CLOCKS_PER_SEC;
-    printf( "Finished fibo naif in %ld s\n", temps ); 
+    double temps = (double)(end-begin)/CLOCKS_PER_SEC;
+    ms = temps * 1000.0;
+    printf( "Finished fibo naif in %f ms\n", ms ); 
     begin = clock();
-    printf("dynamique: %i\n", fibo_dynamique(5));
+    printf("dyna: %i\n", fibo_dynamique(5));
     end = clock();
     temps = (double)(end-begin)/CLOCKS_PER_SEC;
-    printf( "Finished fibo dyna in %ld s\n", temps ); 
+    ms = temps * 1000.0;
+    printf( "Finished fibo dyna in %f ms\n", ms ); 
 
     //prime exo 2
     begin = clock();
-    list_primes_naif(10);
+    list_primes_naif(40);
     
     end = clock();
     temps = (double)(end-begin)/CLOCKS_PER_SEC;
-    printf( "Finished primes naif %ld s\n", temps ); 
+    ms = temps * 1000.0;
+
+    printf( "Finished primes naif %f ms\n", ms ); 
 
     begin = clock();
-    list_primes_efficace(10);
+    list_primes_efficace(40);
     end = clock();
     temps = (double)(end-begin)/CLOCKS_PER_SEC;
-    printf( "Finished primes efficace %ld s\n", temps ); 
+    ms = temps * 1000.0;
+
+    printf( "Finished primes efficace %f ms\n", ms ); 
 
 
 }
